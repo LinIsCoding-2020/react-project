5f01579e33b5cc1122948b4902d9cb97b5746f8c
linjun <774702486@qq.com>
2020-06-09 13:47:07 +0800

    将视图封装成对应的类

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import {
     Route,
     Redirect
 } from "react-router-dom";
-import Layout from "./components/Layout";
+import Money from "./views/Money";
+import Statistics from "./views/Statistics";
+import Tags from "./views/Tags";
+import NoMatch from "./views/NoMatch";
 
 function App() {
     return (
@@ -28,36 +31,5 @@ function App() {
         </Router>
     );
 }
-
-function Tags() {
-    return (
-        <Layout>
-            <h2>标签页面</h2>
-        </Layout>
-    );
-}
-
-function Money() {
-    return (
-        <Layout>
-            <h2>记账页面</h2>
-        </Layout>
-    );
-}
-
-function Statistics() {
-    return (
-        <Layout>
-            <h2>统计页面</h2>
-        </Layout>
-    );
-}
-
-function NoMatch() {
-    return <h2>未找到输入地址，请确认后重新访问</h2>;
-}
-
-
-
 
 export default App;
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoMatch() {
+    return <h2>未找到输入地址，请确认后重新访问</h2>;
+}
+
+export default NoMatch;
