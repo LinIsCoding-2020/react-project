0d49d6ef83c8cf2899e62e0b500ece30e3ffcf82
linjun <774702486@qq.com>
2020-06-10 16:45:40 +0800

    记账页面 样式 和 基础逻辑实现

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,11 +1,49 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useState} from "react";
+import {TagsSection} from "./Money/TagsSection";
+import {RemarkSection} from "./Money/RemarkSection";
+import styled from "styled-components";
+import {CategorySection} from "./Money/CategorySection";
+import {NumberPadSection} from "./Money/NumberPadSection";
+
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`;
+
+type Category = '-' | '+'
 
 function Money() {
+    const [selected, setSelected] = useState({
+        tags: [] as string[],
+        remark: '',
+        category: '-' as Category,
+        amount: '0'
+    });
+    const onChange = (obj: Partial<typeof selected>) => {
+      setSelected({...selected, ...obj});
+    };
+
     return (
-        <Layout>
-            <h2>记账页面</h2>
-        </Layout>
+        <MyLayout>
+            <TagsSection
+                value={selected.tags}
+                onChange={tags => onChange({tags})}
+            />
+            <RemarkSection
+                value={selected.remark}
+                onChange={remark => onChange({remark})}
+            />
+            <CategorySection
+                value={selected.category}
+                onChange={category => onChange({category})}
+            />
+            <NumberPadSection
+                value={selected.amount}
+                onChange={amount => onChange({amount})}
+                onOk={() => {}}
+            />
+        </MyLayout>
     );
 }
 
---
+++ src/views/Money/RemarkSection.tsx
@@ -0,0 +1,49 @@
+import React, {useRef} from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+
+type Props = {
+    value: string,
+    onChange: (value: string) => void
+};
+
+const RemarkSection: React.FC<Props> = (props) => {
+    const remark = props.value;
+    const refInput = useRef<HTMLInputElement>(null);
+    const onBlur = () => {
+        if (refInput.current !== null){
+            props.onChange(refInput.current.value)
+        }
+    }
+    return (
+        <Wrapper>
+            <label>
+                <span>备注</span>
+                <input type="text" placeholder="在这里添加备注"
+                    ref={refInput}
+                    defaultValue={remark}
+                    onBlur={onBlur}
+                />
+            </label>
+        </Wrapper>
+    )
+}
+
+export {RemarkSection}
